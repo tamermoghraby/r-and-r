a3c7442df62932ae315c9a9f0fe14343ca7f673d
tamermoghraby <tamermg8@gmail.com>
2025-12-20 07:28:50 +0200

    using bcryptjs instead of bcrypt

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
